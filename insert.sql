3009fd6176d7f1ce1dd2e4cc0a08e310747ee24a
aresfun-sandler <79336329+aresfun-sandler@users.noreply.github.com>
2021-09-19 14:00:19 +0300

    Add files via upload
    1. Схема прав пользователи и роли. [#1093]

---
+++ insert.sql
@@ -0,0 +1,8 @@
+insert into users(name) values('Oleg');
+insert into role(name) values('Admin');
+insert into rules(name) values('Open');
+insert into category(name) values('Primary');
+insert into state(name) values('Block');
+insert into item(name) values('Opening an account');
+insert into comments(name) values('Successfully');
+insert into attachs(name) values('data file');
