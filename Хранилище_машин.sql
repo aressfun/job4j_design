6cffa65a753135481f5347af4b7bd952b647479b
aressfun <45664383+aressfun@users.noreply.github.com>
2022-04-24 19:42:40 +0300

    Update Хранилище_машин.sql

--- Хранилище_машин.sql
+++ Хранилище_машин.sql
@@ -1,44 +1,46 @@
 create table kuzov(
 	id serial primary key,
-	model varchar(255),
-	id_mashina int references mashina(id)
+	model varchar(255)
 );
 
 create table dvigatel(
 	id serial primary key,
-	model varchar(255),
-	id_mashina int references mashina(id)
+	model varchar(255)
 );
 
 create table korobka_peredach(
 	id serial primary key,
-	model varchar(255),
-	id_mashina int references mashina(id)
+	model varchar(255)
 );
 
 create table mashina(
 	id serial primary key,
-	model varchar(255)
+	model varchar(255),
+	id_korobka_peredach int references korobka_peredach(id),
+	id_kuzov int references kuzov(id),
+	id_dvigatel int references dvigatel(id)
 );
 
-insert into kuzov(id, model, id_mashina) values(1, '1 Series', 1);
-insert into kuzov(id, model, id_mashina) values(2, '2 Series', 2);
-insert into kuzov(id, model, id_mashina) values(3, '3 Series', 3);
-insert into kuzov(id, model, id_mashina) values(4, '14 Series', null);
 
-insert into dvigatel(id, model, id_mashina) values(1, 'Domingo', 1);
-insert into dvigatel(id, model, id_mashina) values(2, 'Dias Wagon', 2);
-insert into dvigatel(id, model, id_mashina) values(3, 'Dex', 3);
-insert into dvigatel(id, model, id_mashina) values(4, 'Dex543', null);
+insert into kuzov(id, model) values(1, '1 Series');
+insert into kuzov(id, model) values(2, '2 Series');
+insert into kuzov(id, model) values(3, '3 Series');
+insert into kuzov(id, model) values(4, '14 Series');
+insert into kuzov(id, model) values(5, '18 Series');
 
-insert into korobka_peredach(id, model, id_mashina) values(1, '6 Series', 1);
-insert into korobka_peredach(id, model, id_mashina) values(2, '7 Series', 2);
-insert into korobka_peredach(id, model, id_mashina) values(3, '8 Series', 3);
+insert into dvigatel(id, model) values(1, 'Domingo');
+insert into dvigatel(id, model) values(2, 'Dias Wagon');
+insert into dvigatel(id, model) values(3, 'Dex');
+insert into dvigatel(id, model) values(4, 'Dex543');
 
-insert into mashina(id, model) values(1, 'Toyota');
-insert into mashina(id, model) values(2, 'Porsche');
-insert into mashina(id, model) values(3, 'BMW');
-insert into mashina(id, model) values(4, 'Vaz');
+insert into korobka_peredach(id, model) values(1, '6 Series');
+insert into korobka_peredach(id, model) values(2, '7 Series');
+insert into korobka_peredach(id, model) values(3, '8 Series');
+
+insert into mashina(id, model, id_korobka_peredach, id_kuzov, id_dvigatel) values(1, 'Toyota', 1, 1, 1);
+insert into mashina(id, model, id_korobka_peredach, id_kuzov, id_dvigatel) values(2, 'Porsche', 2, 2, 2);
+insert into mashina(id, model, id_korobka_peredach, id_kuzov, id_dvigatel) values(3, 'BMW', 3, 3, 3);
+insert into mashina(id, model, id_korobka_peredach, id_kuzov, id_dvigatel) values(4, 'Vaz', null, null, null);
 
 select m.model Машина,
 k.model Кузов,
@@ -46,12 +48,25 @@ d.model Двигатель,
 k_p.model Коробка_передач
 from mashina m 
 left join kuzov k 
-on k.id_mashina=m.id
+on m.id_kuzov=k.id
 left join dvigatel d
-on d.id_mashina=m.id
+on m.id_dvigatel=d.id
 left join korobka_peredach k_p
-on k_p.id_mashina=m.id
+on m.id_korobka_peredach=k_p.id
 
-select*from kuzov where id_mashina is null;
-select*from dvigatel where id_mashina is null;
-select*from korobka_peredach where id_mashina is null;
+select*from kuzov k
+left join mashina m
+on m.id_kuzov=k.id
+where m.id_kuzov is null;
+
+
+select*from dvigatel d
+left join mashina m
+on m.id_dvigatel=d.id
+where m.id_dvigatel is null;
+
+select*from korobka_peredach kb
+left join mashina m
+on m.id_korobka_peredach=kb.id
+where m.id_korobka_peredach is null;
+
